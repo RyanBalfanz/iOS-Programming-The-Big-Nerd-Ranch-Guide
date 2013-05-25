354cd286cbe5890322831b7a01f069f7346a170e
Ryan Balfanz <ryan@ryanbalfanz.net>
2013-05-25 13:50:54 -0700

    Chapter 4; Core Location
    	You also need to know if the CLLocationManager fails to find its location andwhy. If it fails, CLLocationManager sends a different message to its delegate – locationManager:didFailWithError:. In WhereamiViewController.m, implement this method.

--- Whereami/Whereami/WhereamiViewController.m
+++ Whereami/Whereami/WhereamiViewController.m
@@ -40,4 +40,10 @@
     NSLog(@"%@", newLocation);
 }
 
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+    NSLog(@"Could not find location: %@", error);
+}
+
 @end
